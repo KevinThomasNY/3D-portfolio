3ecdbe04b128c7bffb3f7eb366cc169edfada152
Kevin Thomas <kevinthomasny@gmail.com>
2023-05-09 23:18:44 -0400

    added Gandalf model to error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,20 +1,27 @@
 import { useNavigate } from "react-router-dom";
-
+import Gandalf from "./canvas/Gandalf";
 export default function Error() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="mb-8 text-6xl font-bold text-gray-900">Error 404</h1>
-      <p className="mb-8 text-2xl text-gray-700">
+    <div className="flex h-screen flex-col items-center justify-center p-2">
+      <h1 className="mb-8  text-4xl font-bold text-gray-900 md:text-6xl">
+        Error 404
+      </h1>
+      <p className="center mb-8 text-center text-xl text-gray-700 md:text-2xl">
         Oops! The page you're looking for doesn't exist.
       </p>
       <button
-        className="focus:shadow-outline rounded bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+        className="focus:shadow-outline mb-4 rounded bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
         onClick={() => navigate("/")}
       >
         Go to Home Page
       </button>
+      <div className="h-96">
+        <Gandalf />
+      </div>
+
+      <p className="text-2xl text-gray-900 md:text-4xl">YOU SHALL NOT PASS!</p>
     </div>
   );
 }
---
+++ src/components/canvas/Gandalf.jsx
@@ -0,0 +1,46 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Environment,
+  ContactShadows,
+  useGLTF,
+  Preload,
+  OrbitControls,
+} from "@react-three/drei";
+import CanvasLoader from "../Loader";
+const Experience = () => {
+  const druid = useGLTF(
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/druid/model.gltf"
+  );
+
+  return (
+    <>
+      <Environment preset="city" />
+      <OrbitControls />
+      <primitive
+        object={druid.scene}
+        rotation={[0, 0, 0]}
+        position={[0, 0, 1]}
+        scale={1}
+      ></primitive>
+
+      <ContactShadows position-y={0} opacity={0.4} scale={5} blur={2.4} />
+    </>
+  );
+};
+
+export default function Laptop({ theme }) {
+  return (
+    <Canvas
+      camera={{
+        position: [0, 1.6, 2.4],
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <Experience />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+}
