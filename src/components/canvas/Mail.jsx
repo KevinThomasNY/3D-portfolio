c2e2794781e4f18368b3189c6830556509e0c0ff
Kevin Thomas <kevinthomasny@gmail.com>
2024-01-06 21:12:00 -0500

    Fix form input

--- src/components/canvas/Mail.jsx
+++ src/components/canvas/Mail.jsx
@@ -1,17 +1,17 @@
 import React, { Suspense, useRef } from "react";
 import * as THREE from "three";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, Preload } from "@react-three/drei";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 import CanvasLoader from "../Loader";
+
 const Experience = ({ theme }) => {
-  const texture = useRef();
   const sphere = useRef();
-  const loader = new THREE.TextureLoader();
-  const src =
-    theme === "dark"
-      ? "./images/dark_contact_form.jpeg"
-      : "./images/light_contact_form.jpeg";
-  texture.current = loader.load(src);
+  
+  const src = theme === "dark"
+    ? "./images/dark_contact_form.jpeg"
+    : "./images/light_contact_form.jpeg";
+
+  const texture = useLoader(THREE.TextureLoader, src);
 
   const geometry = new THREE.SphereGeometry(500, 60, 40);
   geometry.scale(-1, 1, 1);
@@ -24,7 +24,7 @@ const Experience = ({ theme }) => {
     <>
       <OrbitControls enableZoom={false} rotateSpeed={0.2} />
       <mesh ref={sphere} geometry={geometry} position={[0, 0, 0]}>
-        <meshBasicMaterial map={texture.current} />
+        <meshBasicMaterial map={texture} />
       </mesh>
     </>
   );
@@ -35,7 +35,6 @@ export default function Mail({ theme }) {
     <Canvas>
       <Suspense fallback={<CanvasLoader />}>
         <Experience theme={theme} />
-        <Preload all />
       </Suspense>
     </Canvas>
   );
